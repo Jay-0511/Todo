efa0fe620d9d1b64231930354b3754670549d118
Jay-0511 <jaybalwani12@gmail.com>
2023-04-07 10:47:06 +0530

    error solved

--- src/components/addNode.jsx
+++ src/components/addNode.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useState } from 'react';
 import logo from '../images/logo.png';
 import plus from '../images/plus-icon.png';
 const AddNode = (props)=>{
@@ -7,15 +7,6 @@ const AddNode = (props)=>{
         settextInput(e.target.value)
     }
      const checkTxt= () =>{
-        //  if(text.current.value){
-        //     props.addNode(text.current.value);
-        //     text.current.value = "";
-        //  }
-        //  else{
-        //     text.current.className= "text redborder";
-        //     text.current.value = text.current.value.trim();
-        //      alert("Please enter something in text..");
-        //  }
         if(textInput){
             props.addNode(textInput);
             settextInput("");
